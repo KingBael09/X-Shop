29528fcecc31067d06d6ed7f63db0ab8d58ebbaa
KingBael <www.jayesh567@gmail.com>
2023-08-22 18:17:42 +0530

    feat: fixed VERCEL_URL error

--- src/env.mjs
+++ src/env.mjs
@@ -23,7 +23,7 @@ export const env = createEnv({
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
-    NEXT_PUBLIC_APP_URL: z.string().url(),
+    NEXT_PUBLIC_APP_URL: z.string(),
   },
 
   /**
